22d079e5f4bee5b3f99cb1d4a42c3e6297a4aaa8
Valentin Churavy <v.churavy@gmail.com>
2025-09-04 13:35:53 +0200

    Add an extension for Enzyme

---
+++ ext/EnzymeExt.jl
@@ -0,0 +1,25 @@
+module EnzymeExt
+
+using Enzyme
+using StochasticRounding
+import StochasticRounding: stochastic_round
+
+function Enzyme.typetree_inner(::Type{Float32sr}, ctx, dl, seen::Enzyme.Compiler.TypeTreeTable)
+    return Enzyme.TypeTree(Enzyme.API.DT_Float, -1, ctx)
+end
+
+import Enzyme: EnzymeRules
+import Enzyme: Const, Duplicated
+
+function EnzymeRules.forward(config, ::Const{typeof(stochastic_round)}, ::Type{<:DuplicatedNoNeed}, ::Const{Type{T}}, x::Duplicated) where T
+    stochastic_round(T, x.dval)
+end
+
+function EnzymeRules.forward(config, ::Const{typeof(stochastic_round)}, ::Type{<:Duplicated}, ::Const{Type{T}}, x::Duplicated) where T
+    Duplicated(
+        stochastic_round(T, x.val),
+        stochastic_round(T, x.dval)
+    )
+end
+
+end # module
